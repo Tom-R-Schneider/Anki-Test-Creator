4893fe6cdf0f31c6c58becff5d978508f5d3225a
Tom-R-Schneider <74079909+Tom-R-Schneider@users.noreply.github.com>
2023-03-19 00:34:30 +0100

    Feature add card models to graph (#2)
    * created method to retrieve deck graph
    
    * added a model search for decks/cards
    
    * added model columns to graph

--- parse_anki_file.js
+++ parse_anki_file.js
@@ -1,7 +1,8 @@
 const AdmZip = require("adm-zip");
 const Database = require('better-sqlite3');
 
-anki_file = "anki_example_file/test_for_anki.apkg"
+anki_file = "anki_example_file/All Decks.apkg"
+
 
 const zip = new AdmZip(anki_file);
 var zipEntries = zip.getEntries();
@@ -21,11 +22,15 @@ for (var i = 0; i < zipEntries.length; i++) {
 
 function get_apkg_deck_graph(db) {
 
-    let deck_graph = {};
+    let models = db.prepare("select * from col").all();
+    models = JSON.parse(models[0].models);
+    let deck_graph = {}; // JSON to store final anki deck graph
+    let used_models = {}; // Used to track models and their cleaned up columns to reduce processing steps
     let decks = db.prepare("select decks from col").all();
     decks = JSON.parse(decks[0].decks);
     for (let deck_id in decks) {
-        // Skip default deck as it is not needed
+
+        // Skip default deck as it is not needed for this graph
         if (deck_id == 1) {
             continue;
         }
@@ -40,7 +45,40 @@ function get_apkg_deck_graph(db) {
             }
             curr_branch = curr_branch[deck];
         }
+        
+        // Get card models and columns if there are cards in the current deck
+        card_count = db.prepare("select count (*) from cards where did = '"+ decks[deck_id].id +"'").all();
+        card_count = card_count[0]["count (*)"];
+
+        if (card_count > 0) {
+
+            // One card needed to route from dict to model
+            example_card = db.prepare("select * from cards where did = '"+ decks[deck_id].id +"' limit 0, 1").all();
+            example_card = example_card[0];
+
+            curr_branch["model"] = {
+                "card_count": card_count,
+                "columns": [],
+                "model_used": "" 
+            };
+
+            // Get card model used for dict
+            let card_note = db.prepare("select * from notes where id = '" + example_card.nid + "'").all();
+            card_note = card_note[0];
+            let card_model = models[card_note.mid]
+            curr_branch.model.model_used = card_model.id;    
+            // Push column names into list to use later
+            if (!used_models[card_model.id]) {
+                let columns = [];
+                for (let column of card_model.flds) {
+                    columns.push(column.name);
+                }
+                used_models[card_model.id] = columns;
+            }
+            curr_branch.model.columns = used_models[card_model.id];
+        }    
     }
+
     console.log(deck_graph);
     return deck_graph;
 }
