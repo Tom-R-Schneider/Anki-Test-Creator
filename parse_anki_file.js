a25900af31f6b498b0ca20c759589413b01ee5dd
Tom-R-Schneider <74079909+Tom-R-Schneider@users.noreply.github.com>
2023-04-30 22:01:18 +0200

    UI create learning plan (#5)
    * created parsing function for deck graph into html
    
    * finished simple UI for learning path
    
    * created simple functions for learning plan

--- parse_anki_file.js
+++ parse_anki_file.js
@@ -4,10 +4,15 @@ const initSqlJs = require("sql.js");
 // Required to let webpack 4 know it needs to copy the wasm file to our assets
 //const sqlWasm = require("https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.8.0/sql-wasm.js");
 
+var deck_info = {};
+var selected_decks = {};
+var selected_columns = {};
 
-window.process_file_upload = function(anki_file) {
+
+window.process_file_upload = async function(anki_file, callback) {
     // anki_file = "anki_example_file/All Decks.apkg";
     console.log(anki_file);
+    deck_info = {};
 
     unzipFile(anki_file, async function(fileData) {
         if (fileData) {
@@ -15,6 +20,18 @@ window.process_file_upload = function(anki_file) {
             const db = new SQL.Database(fileData);
             const return_rslt = db.exec("SELECT * FROM col");
             console.log(return_rslt);
+            let deck_graph = window.get_apkg_deck_graph(db);
+            console.log("deck_graph");
+            console.log(deck_graph);
+            let html_graph = window.create_html_from_json(deck_graph);
+            console.log("html_graph");
+            console.log(html_graph);
+            let graphs = {
+                "deck_graph": deck_graph,
+                "html_graph": html_graph,
+                "deck_info": deck_info
+            };
+            callback(graphs);
         }
     
     });
@@ -23,12 +40,14 @@ window.process_file_upload = function(anki_file) {
 
 window.get_apkg_deck_graph = function(db) {
 
-    let models = db.exec("select * from col");
-    models = JSON.parse(models[0].models);
+    let models = db.exec("select models from col");
+    console.log("models");
+    console.log(models[0].values[0]);
+    models = JSON.parse(models[0].values[0]);
     let deck_graph = {}; // JSON to store final anki deck graph
     let used_models = {}; // Used to track models and their cleaned up columns to reduce processing steps
     let decks = db.exec("select decks from col");
-    decks = JSON.parse(decks[0].decks);
+    decks = JSON.parse(decks[0].values[0]);
     for (let deck_id in decks) {
 
         // Skip default deck as it is not needed for this graph
@@ -49,13 +68,15 @@ window.get_apkg_deck_graph = function(db) {
         
         // Get card models and columns if there are cards in the current deck
         card_count = db.exec("select count (*) from cards where did = '"+ decks[deck_id].id +"'");
-        card_count = card_count[0]["count (*)"];
+        console.log(card_count);
+        card_count = card_count[0].values[0][0];
 
         if (card_count > 0) {
 
             // One card needed to route from dict to model
-            example_card = db.exec("select * from cards where did = '"+ decks[deck_id].id +"' limit 0, 1");
-            example_card = example_card[0];
+            let example_card = db.exec("select nid from cards where did = '"+ decks[deck_id].id +"' limit 0, 1");
+            console.log(example_card);
+            let example_card_nid = example_card[0].values[0];
 
             curr_branch["model"] = {
                 "dict_id": decks[deck_id].id,
@@ -66,9 +87,10 @@ window.get_apkg_deck_graph = function(db) {
             };
 
             // Get card model used for dict
-            let card_note = db.exec("select * from notes where id = '" + example_card.nid + "'");
-            card_note = card_note[0];
-            let card_model = models[card_note.mid]
+            let card_note = db.exec("select mid from notes where id = '" + example_card_nid + "'");
+            console.log(card_note);
+            let card_note_mid = card_note[0].values[0];
+            let card_model = models[card_note_mid]
             curr_branch.model.model_used = card_model.id;    
             // Push column names into list to use later
             if (!used_models[card_model.id]) {
@@ -221,4 +243,92 @@ function unzipFile(inputZipFile, callback){
   
     // Read the inputZipFile as an arraybuffer
     reader.readAsArrayBuffer(inputZipFile);
+}
+
+window.create_html_from_json = function(input_json) {
+
+        // Create html string
+        let html_string = "";
+        let counter = 0;
+        html_string += '<li><div>Your Decks</div>';
+        html_string += "<ul>";
+        for (let sub_item_name in input_json) {
+            counter++;
+            html_string += '<li><div><input type="checkbox" onclick="window.handle_deck_click(this)" id="checkbox' + counter + '">' + sub_item_name + '</div>';
+            if (!input_json[sub_item_name].model) {
+            [html_string, counter] = recursive_html_create(input_json[sub_item_name], html_string, counter);     
+            } else {
+                deck_info["checkbox" + counter] = input_json[sub_item_name];
+            }   
+        }
+        html_string += "</ul>";
+        return html_string;
+}
+
+function recursive_html_create(item, html_string, counter) {
+    if (!item.model) {
+        html_string += "<ul>";
+        for (let sub_item_name in item) {
+            counter++;
+            html_string += '<li><div><input type="checkbox" onclick="handle_deck_click(this)" id="checkbox' + counter + '">' + sub_item_name + '</div>';
+            [html_string, counter] = recursive_html_create(item[sub_item_name], html_string, counter);
+        }
+        html_string += "</ul>";
+    } else {
+        console.log("Deck Info");
+        console.log(deck_info);
+        console.log(item);
+        deck_info["checkbox" + counter] = item;
+    }
+    return [html_string, counter];
+
+}
+
+window.get_deck_info_for_id = function(column_id) {
+    let selected_deck = deck_info[column_id];
+    console.log(deck_info);
+    console.log(selected_deck);
+    let columns = selected_deck.model.columns;
+    return columns;
+
+}
+
+window.get_option_html_for_learning_plan = function() {
+    let html_string = "";
+    html_string += "<div><h1>Options</h1></div>";
+    html_string += '<label for="start_date">Start Date:</label><input type="date" id="start_date" name="start_date">';
+    html_string += '<div><input type="radio" id="days_duration" name="learning_duration" value="number of days" checked><label for="days_duration">Number of Days: <input type="number" id="input_days" name="days" min="1"></label></div>';
+    html_string += '<div><input type="radio" id="date_duration" name="learning_duration" value="End Date"><label for="date_duration">End Date: </label><input type="date" id="end_date" name="end_date"></div>';
+    html_string += '<div><input type="checkbox" id="merge_bool" name="merge"><label for="date_duration">Merge decks</label></div>';
+
+
+    return html_string;
+}
+
+window.create_anki_learning_plan = function(decks, columns, merge_bool, start_date, end_date) {
+ 
+}
+
+window.get_checked_columns = function() {
+    return selected_columns;
+}
+
+window.get_checked_decks = function() {
+    return selected_decks;
+}
+
+window.change_checked_deck = function(check_box_id, checked) {
+    if (checked) {
+        selected_decks[check_box_id] = "";
+    } else {
+        delete selected_decks[check_box_id];
+    }
+}
+
+window.change_checked_column = function(check_box_id, checked) {
+    if (checked) {
+        selected_columns[check_box_id] = "";
+    } else {
+        delete selected_columns[check_box_id];
+    }
 }
